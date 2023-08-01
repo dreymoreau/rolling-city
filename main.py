5daa5c3d69795cc5516272f3857b4d7754589673
dreymotoh <striclyspoken@gmail.com>
2023-08-01 10:16:58 -0400

    added check winnings functionality for user, if the symbols match or not

--- main.py
+++ main.py
@@ -11,6 +11,27 @@ COLS = 3
 
 symbol_count = {"A": 2, "B": 4, "C": 6, "D": 8}
 
+symbol_value = {"A": 5, "B": 4, "C": 3, "D": 2}
+
+
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    # checking the bets of how many lines the user bet on
+    for line in range(lines):
+        # we have all the columns, we are storing the first symbol in the symbol variable
+        symbol = columns[0][line]
+        for column in columns:
+            # checking symbol
+            symbol_to_check = column[line]
+            # checking if the symbol is not equal to the next symbol, break
+            if symbol != symbol_to_check:
+                break
+            # if we didnt break out of the loop meaning that the symbols matched, then go to else statement
+        else:
+            winnings += values[symbol] * bet
+
+    return winnings
+
 
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
